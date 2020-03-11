75111e0556fb579ffb2efb1d25800bc58020c35e
Clara <clarayhkim96@gmail.com>
2020-03-11 00:07:36 -0700

    dockerize complete

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+//We will have 2 distinct versions of our app running within 2 separate docker containers on the same machine
 const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("hello world");
+  res.send("hello goodbye");
 });
 
 app.listen(3000, () => {
